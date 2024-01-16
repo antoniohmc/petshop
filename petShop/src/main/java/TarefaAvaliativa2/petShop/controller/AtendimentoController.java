aa98403f70ea9eb9d7ca093775da8a299090e374
tete-30 <hortensia_porto@hotmail.com>
2024-01-16 20:42:22 -0300

    Alteraçoes

--- petShop/src/main/java/TarefaAvaliativa2/petShop/controller/AtendimentoController.java
+++ petShop/src/main/java/TarefaAvaliativa2/petShop/controller/AtendimentoController.java
@@ -4,7 +4,7 @@ import TarefaAvaliativa2.petShop.request.*;
 import TarefaAvaliativa2.petShop.response.AtendimentoResponse;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +17,10 @@ public class AtendimentoController {
     public List<AtendimentoResponse> listarAtendimento(
             @RequestParam(required = false) String estado,
             @RequestParam(required = false) List<String> sort,
-            @RequestParam(required = false) LocalDateTime dataAberturaInicio,
-            @RequestParam(required = false) LocalDateTime dataAberturaFim,
-            @RequestParam(required = false) LocalDateTime dataEncerramentoInicio,
-            @RequestParam(required = false) LocalDateTime dataEncerramentoFim,
+            @RequestParam(required = false) LocalDate dataAberturaInicio,
+            @RequestParam(required = false) LocalDate dataAberturaFim,
+            @RequestParam(required = false) LocalDate dataEncerramentoInicio,
+            @RequestParam(required = false) LocalDate dataEncerramentoFim,
             @RequestParam(required = false) Integer idCliente
     ) {
         return new ArrayList<>();
